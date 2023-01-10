2df30ab530bc0926460bfa864fb33c4ba8a9becf
Karolina Chalupka <k.s.chalupka@gmail.com>
2023-01-10 08:20:21 +0100

    whitespaces added

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Defender.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Defender.java
@@ -3,6 +3,7 @@ package com.codecool.dungeoncrawl.logic.actors;
 import com.codecool.dungeoncrawl.logic.Cell;
 
 public class Defender extends Actor {
+
     public Defender(Cell cell) {
         super(cell);
         setHealth(50);
