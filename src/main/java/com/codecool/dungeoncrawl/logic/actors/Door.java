189f224b0d211fab45a312bd7312c5b20dc64fe0
Karolina Chalupka <k.s.chalupka@gmail.com>
2023-01-10 12:05:13 +0100

    bugs fixed

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Door.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Door.java
@@ -1,8 +1,8 @@
 package com.codecool.dungeoncrawl.logic.actors;
 
-import com.codecool.dungeoncrawl.logic.Cell;
-import com.codecool.dungeoncrawl.logic.CellType;
-import com.codecool.dungeoncrawl.logic.GameMap;
+import com.codecool.dungeoncrawl.logic.map.Cell;
+import com.codecool.dungeoncrawl.logic.map.CellType;
+import com.codecool.dungeoncrawl.logic.map.GameMap;
 import com.codecool.dungeoncrawl.logic.items.Item;
 import com.codecool.dungeoncrawl.logic.items.Key;
 
