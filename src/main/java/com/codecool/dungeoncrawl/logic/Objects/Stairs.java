73996120b5673fdd03659af65ffd6f75a05aa29e
Karolina Chalupka <k.s.chalupka@gmail.com>
2023-01-12 12:53:41 +0100

    MapLoader is as in starter project

--- src/main/java/com/codecool/dungeoncrawl/logic/Objects/Stairs.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/Objects/Stairs.java
@@ -1,19 +1,14 @@
 package com.codecool.dungeoncrawl.logic.Objects;
 
-import com.codecool.dungeoncrawl.gui.Main;
-import com.codecool.dungeoncrawl.logic.items.Item;
-import com.codecool.dungeoncrawl.logic.items.Key;
 import com.codecool.dungeoncrawl.logic.map.Cell;
 import com.codecool.dungeoncrawl.logic.map.CellType;
-import com.codecool.dungeoncrawl.logic.map.GameMap;
-import com.codecool.dungeoncrawl.logic.map.MapFromFileLoader;
+import com.codecool.dungeoncrawl.logic.map.MapLoader;
 
-import java.util.ArrayList;
 import java.util.Objects;
 
 public class Stairs {
     public static void goDown(int dx, int dy, Cell cell) {
-        MapFromFileLoader mapFromFileLoader = new MapFromFileLoader();
+        MapLoader mapFromFileLoader = new MapLoader();
         Cell ogject = cell.getGameMap().getCell(cell.getGameMap().getPlayer().getX() + dx, cell.getGameMap().getPlayer().getY() + dy);
         if (Objects.equals(ogject.getType(), CellType.STAIRSDOWN )){
             cell.getGameMap().getMain().nextLevel();
